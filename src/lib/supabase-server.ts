d7c9d2abbbf20e30e3ea97ad3a4997496e8de44d
Eucode <eucode@MacBook-Air-de-Eucode.local>
2026-03-04 23:32:10 -0300

    teste

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -5,8 +5,8 @@ export async function createClient() {
     const cookieStore = await cookies()
 
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL || "https://fidbarjoswfggjpipmuv.supabase.co",
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpZGJhcmpvd3dmZ2dqcGlwbXUidiIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzQxMTE4NDQ0LCJleHAiOjIwNTY2OTQ0NDR9.5681067952141111111111111111111111111111111111111111111111111111",
         {
             cookies: {
                 getAll() {
